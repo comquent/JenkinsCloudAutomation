24f9da83b3fc539253474a242b2851efbd7250ae
comquent <andreas.schoenfeld@comquent.de>
2017-09-16 15:55:17 +0200

    add grovvy var

--- vars/E2Instance.groovy
+++ vars/E2Instance.groovy
@@ -4,13 +4,21 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest
 import com.amazonaws.services.ec2.model.DescribeInstancesResult
 
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials
+import com.cloudbees.plugins.credentials.CredentialsProvider
+
 def listInstances() {
 
-	def credentials = new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretAccessKey))
-    def client= AmazonEC2ClientBuilder.standard().withCredentials(credentials).build()
-	DescribeInstancesRequest request = new DescribeInstancesRequest()
-	
-	DescribeInstancesResult result = client.describeInstances(request)
-	
-	echo result
+    withCredentials([
+        usernamePassword(credentialsId: params.credentials, usernameVariable: 'accessKey', passwordVariable: 'secretAccessKey')
+    ]) {
+
+		def credentials = new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretAccessKey))
+		def client= AmazonEC2ClientBuilder.standard().withCredentials(credentials).build()
+		DescribeInstancesRequest request = new DescribeInstancesRequest()
+		
+		DescribeInstancesResult result = client.describeInstances(request)
+		
+		echo result
+	}
 }
