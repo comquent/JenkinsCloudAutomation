4c10485f62466c102cbb50a9228f699029c9830c
andreas.schoenfeld@comquent.de <andreas.schoenfeld@comquent.de>
2017-09-16 20:13:36 +0200

    refactor

--- vars/EC2Instance.groovy
+++ vars/EC2Instance.groovy
@@ -1,5 +1,7 @@
 import com.amazonaws.services.ec2.AmazonEC2Client
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder
+import com.amazonaws.services.ec2.model.RunInstancesRequest
+import com.amazonaws.services.ec2.model.RunInstancesResult
 import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
 
